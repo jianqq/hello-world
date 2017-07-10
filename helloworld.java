27e2e0320119abd5c94e93c57a2488347520346a
jianqq <jianqq@glodon.com>
2017-07-10 12:42:33 +0800

    test

--- helloworld.java
+++ helloworld.java
@@ -4,5 +4,6 @@ public class HelloWorld{
   public static void main(String []args){
     System.out.println("My HelloWorld!");
     System.out.println("test");
+      System.out.println("test1");
   }
 }
