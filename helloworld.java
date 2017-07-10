6f1aa0051d73c7a191c458b3f2dc47d8e413c65c
jianqq <jianqq@glodon.com>
2017-07-10 12:38:40 +0800

    test

--- helloworld.java
+++ helloworld.java
@@ -3,5 +3,6 @@ package com.jianqq.hello-world;
 public class HelloWorld{
   public static void main(String []args){
     System.out.println("My HelloWorld!");
+    System.out.println("test");
   }
 }
